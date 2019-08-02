37879c345a89d262cabff00100905b8df6a07136
ivangg1990 <gitprojects90@gmail.com>
2019-08-02 19:05:48 +0200

    First commit

---
+++ src/components/QuantityInput.vue
@@ -0,0 +1,64 @@
+<template>
+    <div class='quantityHolder'>
+        <h3 class="title">Quantity</h3>
+       <form @submit="onSubmit" class='formHolder'>
+           <input type="number"  @change="handleChange" :value='value' class="inputHolder"/>
+           <input type="submit" class="submit-btn"/>
+       </form>
+    </div>
+</template>
+
+<script>
+export default {
+    name: 'QuantityInput',
+    methods: {
+        onSubmit(e) {
+            e.preventDefault()
+            if(this.value < 0){
+                alert('Number must be greater than 0');
+                this.value = null
+            }
+            else{
+                this.$emit('add-value', parseInt(this.value));
+                this.value = null
+            }
+        },
+
+        handleChange(e) {
+            this.value = e.target.value;
+        }
+    },
+
+    data() {
+        return {
+            value: null
+        }
+    }
+}
+</script>
+
+<style scoped>
+    .quantityHolder{
+        width:150px;
+        border:1px solid #666;
+        padding-bottom:15px;
+        margin-left: 50px
+    }
+    .title{
+        color:#666666;
+        text-align:center
+    }
+
+    .formHolder{
+        max-width: 70%;
+        margin-left: 10px;
+    }
+    .inputHolder{
+        max-width:80px;
+        margin-bottom:3px
+    }
+
+    .submit-btn{
+        width: 80px;
+    }
+</style>
---
+++ src/components/SelectList.vue
@@ -0,0 +1,71 @@
+<template>
+  <div class='app-holder'> 
+    <div class="listHolder">
+     <h3 class="title">Quantity</h3>
+     <select @change="handleChange" class='selectInput'>
+        <option v-for='option in selectList' :key="option">{{option === 10 ? option + '+' : option}}</option>
+      </select>
+    </div>
+      <QuantityInput v-if='initValue === 10' @add-value='addNewNum'/>
+  </div>
+</template>
+
+<script>
+
+import QuantityInput from './QuantityInput'
+
+export default {
+  name: 'SelectList',
+  components: {
+    QuantityInput
+  },
+
+  data() {
+    return {
+      selectList : [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10],
+      initValue: 0
+    }
+  },
+
+  methods: {
+    handleChange(e){
+      this.initValue = parseInt(e.target.value);
+    },
+    addNewNum(value){
+      if(!value && value !== 0){
+          alert('Please enter a number')
+          return false         
+        }
+      else if(this.selectList.includes(value)){
+            alert('That number already exists');
+            return false
+        }
+      else{
+          this.selectList = [...this.selectList, value].sort((a,b) => a-b)
+        }
+        return true
+    }
+  }
+}
+</script>
+
+<style scoped>
+  .app-holder{
+    display: flex;
+  }
+  .title{
+    color: #666666;
+  }
+  .listHolder{
+    width: 150px;
+    border:1px solid #666;
+    padding-bottom: 15px;
+    text-align: center;
+}
+.selectInput{
+    max-width: 70%;
+}
+</style>
+
+
+
